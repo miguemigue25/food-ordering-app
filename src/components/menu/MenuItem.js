ef9b735586b668de9b23cdafc3cdb50895ea168e
Miguel <migueortiz0125@gmail.com>
2024-04-29 23:12:43 -0700

    more progress on the menu item details

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -8,6 +8,8 @@ import Image from "next/image";
 export default function MenuItem(menuItem) {
 
     const { image, name, description, basePrice, sizes, extraIngredientPrices } = menuItem;
+    const [selectedSize, setSelectedSize] = useState(null);
+    const [selectedExtras, setSelectedExtras] = useState([]);
     const [showPopup, setShowPopup] = useState(false);
     const { addToCart } = useContext(CartContext);
 
@@ -20,40 +22,77 @@ export default function MenuItem(menuItem) {
         }
     }
 
+    function handleExtraThingClick(e, extraThing) {
+        const checked = e.target.checked;
+        if (checked) {
+            setSelectedExtras(prev => [...prev, extraThing]);
+        } else {
+            setSelectedExtras(prev => {
+                return prev.filter(e => e.name !== extraThing.name);
+            });
+        }
+    }
+
+    let selectedPrice = basePrice;
+    if (selectedSize) {
+        selectedPrice += selectedSize.price;
+    } 
+
+    if (selectedExtras?.length > 0) {
+        for (const extra of selectedExtras) {
+            selectedPrice += extra.price;
+        }
+    }
+
+    selectedPrice = +selectedPrice;
+
     return (
         <>
             {showPopup && (
                 <div className="fixed inset-0 bg-black/80 flex items-center justify-center">
-                    <div className="bg-white p-4 rounded-lg max-w-md">
-                        <Image
-                            src={image}
-                            alt={name}
-                            width={300} height={200}
-                            className="mx-auto" />
-                        <h2 className="text-lg font-bold text-center mb-2">{name}</h2>
-                        <p className="text-center text-gray-500 text-sm mb-2">{description}</p>
-                        {sizes?.length > 0 && (
-                            <div className="bg-gray-200 rounded-md p-2">
-                                <h3 className="text-center text-gray-700">Pick your size</h3>
-                                {sizes.map(size => (
-                                    <label key={size._id} className="flex items-center gap-2 p-4 border rounded-md mb-1">
-                                        <input type="radio" name="size"/> 
-                                        {size.name} ${basePrice} + {size.price}
-                                    </label>
-                                ))}
-                            </div>
-                        )}
-                        {extraIngredientPrices?.length > 0 && (
-                            <div className="bg-gray-200 rounded-md p-2">
-                            <h3 className="text-center text-gray-700">Pick your extra ingredients</h3>
-                            {extraIngredientPrices.map(extraThing => (
-                                <label key={extraThing._id} className="flex items-center gap-2 p-4 border rounded-md mb-1">
-                                    <input type="radio" name="size"/> 
-                                    {extraThing.name} ${extraThing.price}
-                                </label>
-                            ))}
+                    <div className="my-8 bg-white p-2 rounded-lg max-w-md">
+                        <div className="overflow-y-scroll p-2" style={{ maxHeight: 'calc(100vh - 100px' }}>
+                            <Image
+                                src={image}
+                                alt={name}
+                                width={300} height={200}
+                                className="mx-auto" />
+                            <h2 className="text-lg font-bold text-center mb-2">{name}</h2>
+                            <p className="text-center text-gray-500 text-sm mb-2">{description}</p>
+                            {sizes?.length > 0 && (
+                                <div className="bg-gray-200 rounded-md p-2">
+                                    <h3 className="text-center text-gray-700">Pick your size</h3>
+                                    {sizes.map(size => (
+                                        <label key={size._id} className="flex items-center gap-2 p-4 border rounded-md mb-1">
+                                            <input
+                                                type="radio"
+                                                onClick={() => setSelectedSize(size)}
+                                                checked={selectedSize?.name === size.name}
+                                                name="size" />
+                                            {size.name} ${basePrice} + ${size.price}
+                                        </label>
+                                    ))}
+                                </div>
+                            )}
+                            {extraIngredientPrices?.length > 0 && (
+                                <div className="py-2">
+                                    <h3 className="text-center text-gray-700">Pick your extra ingredients</h3>
+                                    {extraIngredientPrices.map(extraThing => (
+                                        <label key={extraThing._id} className="flex items-center gap-2 p-4 border rounded-md mb-1">
+                                            <input 
+                                            type="checkbox" 
+                                            onClick={e => handleExtraThingClick(e, extraThing)}
+                                            name={extraThing.name} />
+                                            {extraThing.name} +${extraThing.price}
+                                        </label>
+                                    ))}
+                                </div>
+                            )}
+                            <button className="primary"
+                                type="button">
+                                Add to cart ${selectedPrice}
+                            </button>
                         </div>
-                        )}
                     </div>
                 </div>
             )}
